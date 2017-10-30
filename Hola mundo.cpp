357b1ed5a5f9bcf4bf56785813e5a77298e1ed06
Kike2191 <jeramirez1105@gmail.com>
2017-10-30 11:51:39 -0600

    Actualizacion 1

--- Hola mundo.cpp
+++ Hola mundo.cpp
@@ -4,8 +4,13 @@ using namespace std;
 
 int main()
 {
+	int a, b;
 cout<<"hola mundo!\n";
 cout<<"comentarios";
+cout<<"ingresa numero 1\n";
+cin>>a;
+cout<<"ingresa numero 2\n";
+cin>>b;
 
 
  system("pause");
